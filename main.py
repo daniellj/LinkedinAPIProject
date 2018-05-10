bb1f79630cf58755aeafac9daab3196046e7c979
Daniel L.J <daniellessa.j@gmail.com>
2018-05-10 16:46:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,20 @@
 @author: daniellj
 """
 
-from linkedin import search_company_by_vanityName
-from company import Company
+import company.Company
 
+
+# Criando uma lista de empresas
 company_list = ['Gerdau', 'Agibank', 'Multiplan', 'Petrobras', 'Google']
-#r = oauth.get('https://api.linkedin.com/v2/organizations?q=vanityName&vanityName=devtestco1')
 
+# chamando a função para efetuar a consulta das empresas
 for comp in company_list:
     search_company_by_vanityName(comp)
-    
+
 # mapear os valores de saída da função para os atributos da classe "Company".
 
+
 # imprimir os valores dos atributos da classe  
+
+
+# gerar arquivo JSON saída
