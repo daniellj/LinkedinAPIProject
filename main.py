bbab715b8af0d4ef0be8a006027bda8c585322b6
Daniel L.J <daniellessa.j@gmail.com>
2018-05-14 13:40:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,30 @@
 """
 @author: daniellj
 """
+import json
+from company import Company
 
-import company.Company
+if __name__ == "__main__":
+    # Ajustando o seletor de campos de retorno
+    selectors = ['entityStatus','vanityName','id','industries','foundedOn','website','specialties','staffCountRange']
+    extracted_data = []
+    
+    # Criando uma lista de empresas
+    #company_list = ['Gerdau', 'Agibank', 'Multiplan', 'Petrobras', 'Google']
+    company_list = ['devtestco1']
+    
+    for comp in company_list:
+        # chamando a função para efetuar a consulta das empresas er colocando o resultado em uma lista
+        extracted_data.append(Company.search_company_by_vanityName(vanityName=comp, selectors=selectors))
 
-
-# Criando uma lista de empresas
-company_list = ['Gerdau', 'Agibank', 'Multiplan', 'Petrobras', 'Google']
-
-# chamando a função para efetuar a consulta das empresas
-for comp in company_list:
-    search_company_by_vanityName(comp)
-
-# mapear os valores de saída da função para os atributos da classe "Company".
-
-
-# imprimir os valores dos atributos da classe  
-
-
-# gerar arquivo JSON saída
+    if extracted_data:
+        print('')
+        print('ERRO: conteúdo retornado vazio -->>', extracted_data)
+    else:
+        print('')
+        print('Resultado da requisição em tela:', extracted_data)
+        print('')        
+        print('Exportando resultado da requisição para o arquivo data.json...')
+        f = open('data.json', 'w')
+        json.dump(extracted_data, f, indent=4)
+        print('Exportação finalizada com sucesso!')
